aad8da9e7f1c426925c3f9e30ee7e6f7c383560e
Moisés Neto <ntocampos@gmail.com>
2023-11-10 14:38:12 +0000

    Adds Tailwind to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>React + Vite</h1>
-      <h2>On CodeSandbox!</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR.
-        </p>
-
-        <p>
-          Tip: you can use the inspector button next to address bar to click on
-          components in the preview and open the code in the editor!
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <main className="p-8">
+      <h1 className="font-semibold mb-4">Overlapr Demo</h1>
+      <p className="mb-4">
+        Sunt est ullamco ea commodo labore et cupidatat ullamco occaecat culpa
+        proident ea qui nostrud. Officia dolor veniam dolor aliquip proident
+        aute nulla. Quis ea esse laboris velit ex id ut aute. Laborum anim
+        labore cillum qui amet consequat velit in ipsum occaecat magna. Laboris
+        sit Lorem proident culpa duis velit. Magna do excepteur in consequat
+        sint commodo irure fugiat tempor sunt.
       </p>
-    </div>
+    </main>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["ui-sans-serif", "system-ui"],
+      serif: ["ui-serif", "Georgia"],
+      mono: ["ui-monospace", "SFMono-Regular"],
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
